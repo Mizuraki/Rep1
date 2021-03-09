0eaf7b75d4fbccc7184d6e71127e092238819bf7
Mizuraki <71286251+Mizuraki@users.noreply.github.com>
2021-03-09 18:36:54 +0300

    Add Bot

---
+++ config.py
@@ -0,0 +1,10 @@
+TOKEN = '1402656916:AAF-tcgBKx-dC-QnNN6OhVejpEH3VLvGUVc'
+
+keys = {
+    'доллар': 'USD',
+    'Доллар': 'USD',
+    'евро': 'EUR',
+    'Евро': 'EUR',
+    'рубль': 'RUB',
+    'Рубль': 'RUB',
+}
